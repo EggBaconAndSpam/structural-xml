f661451ba790b4b39676f4abf88e292d2ea634fc
Frederik Ramcke <frederik.ramcke@eirforsakring.se>
2024-03-17 11:06:19 +0100

    Allow all  dates

--- iso20022-structural-xml/src/Data/XML/ISO20022/Types.hs
+++ iso20022-structural-xml/src/Data/XML/ISO20022/Types.hs
@@ -1,23 +1,31 @@
 module Data.XML.ISO20022.Types where
 
+import Control.Applicative
 import qualified Data.Text as Text
 import Data.Time
 import Data.Time.Format.ISO8601
 import Data.XML
 
--- ISO8601 timezoned (xs:dateTime technically allows non-timezoned but that's ridiculous)
-newtype ISODateTime = ISODateTime {unISODateTime :: UTCTime}
+-- xs:dateTime allows both timezoned and non-timezoned...
+data ISODateTime = ISODateTime UTCTime | ISODateTimeLocal LocalTime
   deriving (FromElement, ToElement) via ContentElement ISODateTime
 
 instance FromContent ISODateTime where
-  fromContent t i = case iso8601ParseM $ Text.unpack t of
-    Nothing -> Left . parserError i $ "Not a valid timestamp in ISO8601 format: " <> Text.unpack t
-    Just u -> pure . ISODateTime $ zonedTimeToUTC u
+  fromContent t i = do
+    let t' = Text.unpack t
+    maybe
+      (Left . parserError i $ "Not a valid timestamp in ISO8601 format: " <> Text.unpack t)
+      pure
+      ( ISODateTime . zonedTimeToUTC <$> iso8601ParseM t'
+          <|> ISODateTime <$> iso8601ParseM t'
+          <|> ISODateTimeLocal <$> iso8601ParseM t'
+      )
 
 instance ToContent ISODateTime where
   toContent (ISODateTime t) = Text.pack $ iso8601Show t
+  toContent (ISODateTimeLocal t) = Text.pack $ iso8601Show t
 
--- ISO8601 Day (xs:date also allows timezones)
+-- ISO8601 Day (xs:date also allows timezones but ISO20022 doesn't)
 newtype ISODate = ISODate {unISODate :: Day}
   deriving (FromElement, ToElement) via ContentElement ISODate
 
@@ -29,7 +37,7 @@ instance FromContent ISODate where
 instance ToContent ISODate where
   toContent (ISODate d) = Text.pack $ iso8601Show d
 
--- xs:boolean also allows 0/1
+-- xs:boolean also allows 0/1?
 newtype ISOBool = ISOBool {unISOBool :: Bool}
   deriving (FromElement, ToElement) via ContentElement ISOBool
 
